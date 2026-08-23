ab11ae2ebd2f04cce03e3d5906572414aa8c2a55
DESKTOP-2EP5TF7\User <202210172@ua.edu.lb>
2026-08-23 21:13:39 +0300

    Fix Laravel CORS middleware

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,6 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
 
+        $middleware->use([
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
+
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
