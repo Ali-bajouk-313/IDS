cb3f526add99ddfe230a6566a0e1a6b68cba02a1
DESKTOP-2EP5TF7\User <202210172@ua.edu.lb>
2026-08-23 21:16:12 +0300

    Register CORS middleware correctly

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,9 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
 
-        $middleware->use([
-            \Illuminate\Http\Middleware\HandleCors::class,
-        ]);
+        $middleware->append(
+            \Illuminate\Http\Middleware\HandleCors::class
+        );
 
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
