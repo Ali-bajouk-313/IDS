f4e9681e9ea58da7c7837b666e35f3c028bc814a
DESKTOP-2EP5TF7\User <202210172@ua.edu.lb>
2026-08-23 22:22:51 +0300

    Fix CORS middleware priority

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
 
-        $middleware->append(
+        $middleware->prepend(
             \Illuminate\Http\Middleware\HandleCors::class
         );
 
